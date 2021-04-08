d984fd8434c49ff727b06412df8d4a84bd63a478
miguellvc <miguellrvc@gmail.com>
2021-04-07 21:31:30 -0300

    comenzando a trabajar con js

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+/*
+    código de las categorías
+    novedades: 100
+    Productos Destacados: 101
+    Lo más visto: 102
+
+*/
+categoryProduct = ['Novedades', 'Productos Destacados', 'Lo más visto']; 
+
+products = [
+    {
+        id:         "001",
+        idCategory: 101,
+        title:      "Memoria Ram DDR4 16GB",
+        price:      "4.500",
+        start:      "5",
+    },
+    {
+        id:         "002",
+        idCategory: 101,
+        title:      "Memoria Ram DDR4 16GB",
+        price:      "4.500",
+        start:      "5",
+    },
+    {
+        id:         "003",
+        idCategory: 101,
+        title:      "Memoria Ram DDR4 16GB",
+        price:      "4.500",
+        start:      "5",
+    }
+]
+
+console.log(products); 
+for(i=0;  i < categoriaProducto.length; i++){
+
+    console.log(categoriaProducto[i]);
+    
+}
+
+// console.log(categoriaProducto); 
